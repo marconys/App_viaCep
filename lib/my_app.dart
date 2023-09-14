f61e4bdfeec0034ff40ce4db0858052a623b32db
marconys <marconysbatera@gmail.com>
2023-09-14 13:45:14 -0300

    feat: adjusting the initial structure of the app

---
+++ lib/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:via_cep_api/views/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple, textTheme: GoogleFonts.robotoCondensedTextTheme()),
+      home: const HomeView(),
+    );
+  }
+}
